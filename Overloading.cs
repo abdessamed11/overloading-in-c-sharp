4b5cbfa9cdbe412d9030cf11887f324a92ee92f6
Administrateur <Administrateur@DESKTOP-QUDI77S>
2020-12-10 11:43:40 +0100

    Ajoutez des fichiers projet.

---
+++ Overloading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace overloading_in_c_sharp
+{
+    class Overloading
+    {
+        public int add(int a, int b)
+        {
+            int sum= a+b;
+            return sum;
+        }
+
+        public int add(int a,int b,int c)
+        {
+            int sum1 = a + b + c;
+            return sum1;
+        }
+
+        public float add(float a, float b)
+        {
+            float sum = a * b;
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace overloading_in_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Overloading over = new Overloading();
+            int samm = over.add(10,20);
+            Console.WriteLine(samm);
+
+            int sam2 = over.add(10, 2, 5);
+            Console.WriteLine(sam2);
+
+            float sam3 = over.add(10.4f, 20.3f);
+            Console.WriteLine(sam3);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
